52a2fffca900b67047b0adf435072d2d5afc88b9
AlvinLee31 <65617371+AlvinLee31@users.noreply.github.com>
2023-07-28 13:45:42 -0700

    Update command_line.c

--- command_line.c
+++ command_line.c
@@ -1,31 +1,34 @@
 Terminal/ Command Line Interface Commands()
 {
+pwd     : Present Working Directory // pwd 
 ls      : List                      // ./ls  
 cd      : Change directory          // cd ../../..  or cd user/admin/desktop/lecture_notes 
-pwd     : Present Working Directory // pwd 
 ./      : run executable            // ./pset1*
 mkdir   : make directory            // mkdir newfolder
-cp      : Copy                      // cp <source> <destination> // cp file.c copy.c 
+    mkdir -p: add string name       // mkdir -p "new folder"
+cp      : Copy                      // cp <source> <destination> || cp file.c copy.c 
     cp -r   : Copy Folder Recursively   // cp -r folder1 folder2   //Directories must be copied recursively to include its contents
-
 rm      : remove                    // rm file.c
     rm -f : force remove (skip y/n confirmation)
-    rm -r : recursively delete things in directory 
-    rm -rf: both 
+    rm -r : recursively delete things in directory (for folders with files in them) 
+    rm -rf: both
 rmdir   : remove directory          // rmdir newfolder
-mv      : Move/ Rename              // mv <source> <destination> // mv rename.c renamed.c (works on directories too) | mv rename.c folder/ (moves file into a different folder) // It makes a copy of the file, then deletes the old one. 
-    mv -r   : Move recursively 
+    rmdir -r: remove recursively
+mv      : Move/ Rename              // mv <source> <destination> || mv rename.c renamed.c (works on directories too) | mv rename.c folder/ (moves file into a different folder) // It makes a copy of the file, then deletes the old one. 
+    mv -r   : Move recursively
 }
+
 
 Advanced Commands()
 {
 chmod   : 
 ln      :
-touch   :
+touch   : makes a new file          // touch code.c  
 man     : 
 diff    :
-sudo    : 
+sudo    : run as admin
 telnet  : 
+clear   : clear terminal (ctrl + l) 
 }
 
 
